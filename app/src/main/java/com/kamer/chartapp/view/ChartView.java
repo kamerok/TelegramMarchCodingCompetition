948b56fd034c0571f4f9e718f6efee8dc3338b8f
Maksim Dvoretskii <kamerok@gmail.com>
2019-03-13 17:03:17 +0300

    Add chart view

---
+++ app/src/main/java/com/kamer/chartapp/view/ChartView.java
@@ -0,0 +1,48 @@
+package com.kamer.chartapp.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class ChartView extends View {
+
+    private Paint paint;
+
+    public ChartView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ChartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setColor(Color.RED);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0f, 0f, 50f, 50f, paint);
+    }
+}
