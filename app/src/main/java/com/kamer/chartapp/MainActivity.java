08b0eaf68f424e3210cc473869e9c9b0c23d5519
Maksim Dvoretskii <kamerok@gmail.com>
2019-03-14 10:27:49 +0300

    Change controls

--- app/src/main/java/com/kamer/chartapp/MainActivity.java
+++ app/src/main/java/com/kamer/chartapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.kamer.chartapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.kamer.chartapp.view.ChartView;
 import com.kamer.chartapp.view.data.InputItem;
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Random random;
     private ChartView chartView;
+    private SeekBar leftView;
+    private SeekBar rightView;
+    private SeekBar panView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,58 @@ public class MainActivity extends AppCompatActivity {
 
         random = new Random();
         chartView = findViewById(R.id.view_chart);
+        leftView = findViewById(R.id.view_left_border);
+        rightView = findViewById(R.id.view_right_border);
+        panView = findViewById(R.id.view_pan);
 
+        leftView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("TAG", "left: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        rightView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("TAG", "right: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        panView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("TAG", "pan: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         chartView.post(new Runnable() {
             @Override
             public void run() {
@@ -50,21 +106,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void onReloadClick(View view) {
         reloadData();
-    }
-
-    public void onIncreaseZoomClick(View view) {
-        chartView.setZoom(chartView.getZoom() + 0.1f, true);
-    }
-
-    public void onDecreaseZoomClick(View view) {
-        chartView.setZoom(chartView.getZoom() - 0.1f, true);
-    }
-
-    public void onIncreasePanClick(View view) {
-        chartView.setPan(chartView.getPan() + 0.1f);
-    }
-
-    public void onDecreasePanClick(View view) {
-        chartView.setPan(chartView.getPan() - 0.1f);
     }
 }
